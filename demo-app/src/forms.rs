1a24c1b3e595935c840aa1e087471005eb93b7b1
itsuki <alliotsebastien04@gmail.com>
2026-01-29 02:13:43 +0100

    leger css pour changer un peux

--- demo-app/src/forms.rs
+++ demo-app/src/forms.rs
@@ -94,31 +94,32 @@ impl RuniqueForm for Blog {
         // Titre (Texte simple)
         form.field(
             &TextField::text("title")
-                .placeholder("Entrez un titre accrocheur")
+                .label("Entrez un titre accrocheur")
                 .required("Le titre est obligatoire"),
         );
 
         // Email de l'auteur
         form.field(
             &TextField::email("email")
-                .placeholder("Entrez l'email de l'auteur")
+                .label("Entrez l'email de l'auteur")
                 .required("L'email est requis"),
         );
 
         // Site Web (URL)
-        form.field(&TextField::url("website").placeholder("Entrez le site web source"));
+        form.field(&TextField::url("website")
+                .label("Entrez le site web source"));
 
         // Résumé (TextArea)
         form.field(
             &TextField::textarea("summary")
-                .placeholder("Un court résumé...")
+                .label("Un court résumé...")
                 .required("Veuillez fournir un résumé"),
         );
 
         // Contenu (TextArea ou RichText si implémenté dans GenericField)
         form.field(
             &TextField::richtext("content")
-                .placeholder("Entrez le contenu de l'article ici")
+                .label("Entrez le contenu de l'article ici")
                 .required("Le contenu ne peut pas être vide"),
         );
     }
