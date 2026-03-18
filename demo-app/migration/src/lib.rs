191109bc8f837888f8a955649c8b85904734de1b
seb-alliot <sebastienalliot04@gmail.com>
2026-03-18 15:19:30 +0100

    bug relation table ordre de creation de migrations

--- demo-app/migration/src/lib.rs
+++ demo-app/migration/src/lib.rs
@@ -1,10 +1,4 @@
 pub use sea_orm_migration::prelude::*;
-mod m20260318_140408_create_blog_table;
-mod m20260318_140408_create_contributions_table;
-mod m20260318_140408_create_eihwaz_users_table;
-mod m20260318_140408_create_test_all_fields_table;
-mod m20260318_140408_create_users_booster_table;
-mod m20260318_140518_alter_contributions_table;
 
 pub struct Migrator;
 
@@ -15,12 +9,6 @@ impl MigratorTrait for Migrator {
         //     .into_iter()
         //     .collect();
         vec![
-            Box::new(m20260318_140408_create_blog_table::Migration),
-            Box::new(m20260318_140408_create_contributions_table::Migration),
-            Box::new(m20260318_140408_create_users_booster_table::Migration),
-            Box::new(m20260318_140408_create_test_all_fields_table::Migration),
-            Box::new(m20260318_140408_create_eihwaz_users_table::Migration),
-            Box::new(m20260318_140518_alter_contributions_table::Migration),
         ]
     }
 }
