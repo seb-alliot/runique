ee6cbaaf79289c450c78e49df008929a6b6539b6
seb-alliot <sebastienalliot04@gmail.com>
2026-04-16 16:09:36 +0200

    incompatible ?

--- demo-app/src/main.rs
+++ demo-app/src/main.rs
@@ -46,7 +46,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .with_header_security(true)
                         .with_upgrade_insecure(!is_debug())
                         .scripts(vec![
-                            "'strict-dynamic'", 
+                            "'self'", 
                             "https://www.googletagmanager.com", 
                             "https://www.google-analytics.com "
                             ])
